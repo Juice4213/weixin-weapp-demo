74ff8f738a1da1304bb2f26fa36ca0a90f427f94
Juice_gg <572101134@qq.com>
2017-06-01 19:08:28 +0800

    删除文档

--- pages/leftSwiperDel/index.js
+++ pages/leftSwiperDel/index.js
@@ -112,13 +112,25 @@ Page({
     });
   },
 
-  touchS (e ) {
+  touchS (e){
+    // console.log('触摸开始：',e);
+    if(e.touches.length == 1){
+      this.setData({
+        //设置触摸起始点水平方向位置
+        startX: e.touches[0].clientX
+      })
+    }
+  },
+
+  touchM (e ) {
+    // console.log('触摸移动：',e);
     if(e.touches.length == 1){
       //手指移动时水平方向位置
       let moveX = e.touches[0].clientX;
       //手指起始点位置与移动期间的差值
       let disX = this.data.startX - moveX;
       let delBtnWidth = this.data.delBtnWidth;
+      console.log('delBtnWidth1111:', delBtnWidth);
       let txtStyle = "";
       if(disX == 0 || disX < 0){
         //如果移动距离小于等于0， 文本层位置不变
@@ -127,7 +139,7 @@ Page({
         //移动距离大于0，文本层left值等于手指移动距离
         txtStyle = "left:-" + disX + "px";
         if(disX >= delBtnWidth){
-          //控制手指移动距离最大值为删除按钮的高度
+          //控制手指移动距离最大值为删除按钮的宽度
           txtStyle = "left:-"+delBtnWidth+"px";
         }
 
@@ -143,5 +155,33 @@ Page({
         }
       }
     }
+  },
+
+  touchE (e){
+    console.log('触摸结束：',e);
+    if(e.changedTouches.length == 1){
+      //手指移动结束后水平位置
+      let endX = e.changedTouches[0].clientX;
+      console.log('endX:',endX);
+      //触摸开始于结束， 手指移动的距离
+      let disX = this.data.startX - endX;
+      console.log('disX:',disX);
+      let delBtnWidth = this.data.delBtnWidth;
+      console.log('delBtnWidth:', delBtnWidth);
+      //如果距离小于删除按钮的1/2，不显示删除按钮
+      let txtStyle = disX > delBtnWidth/2 ? "left:-"+delBtnWidth+"px":"left:0px";
+      console.log('txtStyle::',txtStyle);
+      //获取手指触摸的是哪一项
+      let index = e.target.dataset.index;
+      let list = this.data.list;
+      if(index){
+        list[index].txtStyle = txtStyle;
+        //更新列表的状态
+        this.setData({
+          list:list 
+        })
+      }
+    }
   }
+
 })
